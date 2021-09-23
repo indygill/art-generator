b2f9c67a61039dd7f67e69bb46835a4253c62f57
Indy Gill <hello@indygill.com>
2021-09-23 22:32:06 +0000

    Changed parameters Added method to provide a number between -1 and 1. Defaults to 0.618 for now

--- src/Actions/GenerateImagesAction.php
+++ src/Actions/GenerateImagesAction.php
@@ -9,8 +9,7 @@ class GenerateImagesAction
     protected $command_path = 'neural-net-random-art/random_art.py';
     protected $img_width = 512;
     protected $img_height = 512;
-    protected $colormode = 'RGB';
-    protected $alpha = 'false';
+    protected $activation = 'softsign';
 
     public function execute(Int $images = 1)
     {
@@ -21,13 +20,17 @@ class GenerateImagesAction
             $this->img_height,
             '-img_width',
             $this->img_width,
-            '-colormode',
-            $this->colormode,
-            '-alpha',
-            $this->alpha,
+            '-activation',
+            $this->activation,
+            '-z1',
+            $this->getRandomDimension(),
+            '-z2',
+            $this->getRandomDimension(),
             '-n_images',
             $images
         ]);
+
+        $process->setTimeout(60 * 10);
 
         $process->run();
 
@@ -42,5 +45,14 @@ class GenerateImagesAction
     public function setImgHeight(Int $height)
     {
         $this->img_height = $height;
+    }
+
+    protected function getRandomDimension()
+    {
+        // rand()/getrandmax() gives a float number between 0 and 1
+        // if you multiply it by 1 you'll get a number between 0 and 1
+        // add the starting number -0.99 and you'll get a number between -0.99 and 1
+        // https://stackoverflow.com/questions/3206563/how-to-generate-a-random-positive-or-negative-decimal
+        return 0.618;//rand() / getrandmax() * 1 - 0.99;
     }
 }
